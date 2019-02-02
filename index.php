9c598b395f8840ed907a5a53c7ddda1623a51636
slur-dev <Taa_wappey@windowslive.com>
2019-02-02 22:59:46 +0700

    update index page

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <!-- <meta http-equiv="refresh" content="0;url="> -->
-<title>SB Admin 2</title>
-<script language="javascript">
-    window.location.href = "src/pages/index.php"
-</script>
+<title>SB Admin 2 XX</title>
+<!--    Style Sheet -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="src/assets/css/style.css">
 </head>
 <body>
-Go to <a href="src/pages/index.php">src/pages/index.php</a>
+<div class="header">
+    <?php include("src/components/header.php"); ?>
+</div>
+<div class="content">
+    <div class="body_c">
+        <?php include("src/components/content.php"); ?>
+    </div>
+</div>    
+<div class="footer">
+<?php include("src/components/footer.php"); ?>
+</div>
 </body>
+
+<!-- Script -->
+<script src="src/assets/Js/header.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!-- <script language="javascript"> window.location.href = "src/pages/index.php"</script> -->
 </html>
--- src/components/header.php
+++ src/components/header.php
@@ -1,22 +1,53 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark-green fixed-top">
-  <div class="container">
-    <a class="navbar-brand active" href="/moosi-massage">Moosi Massage</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-      </ul>
-    </div>
+<div>
+<div>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:pink">
+  <a class="navbar-brand" href="#">แนวแผน ไทยหมู่สี</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">จองคิวหมอนวด<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">สินค้า</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ดูข้อมูลรำยกำรนวดและรำคำค่ำบริกำร</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      <i class="fab fa-user"></i>
+      เข้าสู่ระบบ
+    </span>
   </div>
 </nav>
+</div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="carousel_c d-block w-100" src="src/assets/images/spa-slide1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="carousel_c d-block w-100" src="src/assets/images/spa-slide2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="carousel_c d-block w-100" src="src/assets/images/spa-slide3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
