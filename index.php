1cfad15eb6760f6757b38846b75cd254ac5e8ca9
kittikunb <kittikunb@wisesoft.co.th>
2019-02-03 17:56:17 +0700

    Insert user

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!-- <meta http-equiv="refresh" content="0;url="> -->
+<meta http-equiv="refresh" content="0;url=src/pages/admin.php">
 <title>SB Admin 2</title>
 <script language="javascript">
-    window.location.href = "src/pages/index.php"
+    window.location.href = "src/pages/admin.php"
 </script>
 </head>
 <body>
-Go to <a href="src/pages/index.php">src/pages/index.php</a>
+Go to <a href="src/pages/admin.php">src/pages/admin.php</a>
 </body>
 </html>
